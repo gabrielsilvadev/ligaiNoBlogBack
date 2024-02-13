8a8e86c8d34a5e8e41b4e50ad401b21828e3ca55
João Gabriel da Silva <55601337+gabrielsilvadev@users.noreply.github.com>
2024-02-13 15:06:23 -0300

    ajuste cors
    ajuste

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,11 @@ import express from 'express';
 import './database/conection';
 import 'express-async-errors'
 import routes from './routes';
+import cors from "cors"
 
 const app = express();
 app.use(express.json());
+app.use(cors())
 app.use(routes);
 const port = process.env.PORT || 3333;
 app.listen(port, () => {
